a6f74bd3b9b6c43eafd1a05f5bdfc3f79a9d3d79
Alternade <aptsahu2@gmail.com>
2021-10-29 18:01:22 +0530

    Added Part time feature

--- EmployeeAttendence.sh
+++ EmployeeAttendence.sh
@@ -1,11 +1,16 @@
 
 isPresent=1
+isPartTime=2
 salPerHr=20
-random=$((RANDOM%2))
+random=$((RANDOM%3))
 if [ $random -eq $isPresent ]
 then 
 	echo "Employee is Present"
 	empHr=8
+elif [ $random -eq $isPartTime ]
+then 
+	echo "Employee is present for part time"
+	emphr=4;
 else
 	echo "Employee is Absent"
 	empHr=0
